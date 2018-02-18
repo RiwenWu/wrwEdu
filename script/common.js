b7ecd9dfe6bd752f8dada5c31fe0465567e20b7d
517106121@qq.com <517106121@qq.com@0e1a2743-8d03-4e5a-a60c-7e26af42b145>
2018-02-18 08:33:58 +0000

    完成首页类似流加载
    git-svn-id: svn://svn3.apicloud.com/A6069095784371/wrwEdu@4 0e1a2743-8d03-4e5a-a60c-7e26af42b145

--- script/common.js
+++ script/common.js
@@ -1,8 +1,10 @@
 //判断是否已登陆
 function isLogin() {
-  if ($api.getStorage('islogin') == undefined) {
+  if ($api.getStorage('userId') == undefined) {
       window.console.log('islogin-------------false');
+      return false;
   } else {
       window.console.log('islogin-------------true');
+      return true;
   }
 }
